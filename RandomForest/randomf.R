f663f7d7c933ae388d3a093cf4c5c0ef65689b54
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2025-06-27 14:45:14 +0200

    testing

--- RandomForest/randomf.R
+++ RandomForest/randomf.R
@@ -30,86 +30,86 @@ library(tidypredict)
 # train <- dalong[ind==1,]
 # test <- dalong[ind==2,]
 
-ttsplit <- function(x, split){
-  ind <- sample(2, nrow(x), 
-                replace = TRUE, 
-                prob = split)
-  train <- x[ind==1,]
-  test <- x[ind==2,]
-  return(list(train=train, test=test))
-}
-
-
-# Create random binary data from which to select
-# observations, `Nsets` is number of 
-# data sets to create (always keep at 1)
-# `N` is number of observations, `varnum` is number of variables.
-# Worth considering increasing `N`. Increasing `varnum` results
-# in more irrelevant factors later on.
-varnum <- 18
-outcome <- "A"
-base <- as.data.table(bs_dat_create(Nsets = 1, N = 100000, varnum = varnum)[[1]])
-
-
-# Set variable names if more vars than length(LETTERS)
-if (length(LETTERS) < varnum){
-  names(base) <- c(LETTERS, sapply(1:(varnum-length(LETTERS)), 
-                                   \(x) paste0("U", x)))
-}
-
-# Have a look, if you want
-# head(base)
-
-# Create random binary CNA models for "A",
-# to be used as targets. 
-# `relevants`:=number of factors included in the models.
-relevants <- 15
-targets <- replicate(40, rasf_hack(LETTERS[1:relevants], 
-                                    outcome = "A",
-                                    max.conj = 6,
-                                    neg.prob = 0.5))
-
-
-# Corresponding to each target, create a "clean" data set where 
-# each observation is some configuration of variable values
-# that conforms to the target, selected from `base`.
-# Note that the irrelevant factors
-# may take any values whatsoever.
-# The way the targets are created results in ideal data
-# with extreme outcome prevalence. The function below
-# corrects for this by oversampling cases with
-# outcome absent and forcing desired outcome prevalence.
-alt_SC <- function(mod,data,out,preval){
-  a <- ct2df(selectCases(mod, data))
-  n <- nrow(a)
-  nout <- sum(a[out])
-  o_prev <- nout / n
-  if(o_prev > preval){
-    nn <- preval*n
-    to_add <- ceiling((nout - nn) / preval)
-    b <- ct2df(selectCases(mod, data[get(out)==0,]))
-    neg_outs <- b[sample(1:nrow(b), to_add, replace = TRUE),]
-    a <- rbind(a, neg_outs)
-  }
-  return(a)
-} 
-
-
-#cleandats <- lapply(targets, \(x) ct2df(selectCases(x, base)))
-
-# Noise-free data sets w/ outcome prevalence =<80%
-cleandats <- lapply(targets, \(x) alt_SC(x, base, outcome,0.8))
-
-# Check that outcome prevalence not zero and discard
-# data set / target if that is the case
-check_vars <- function(x){
-  vars <- unlist(lapply(x, \(y) var(as.numeric(y[,outcome]))))
-  return(which(vars > 0))
-}
-
-keep <- check_vars(cleandats)
-targets <- targets[keep]
-cleandats <- cleandats[keep]
+# ttsplit <- function(x, split){
+#   ind <- sample(2, nrow(x), 
+#                 replace = TRUE, 
+#                 prob = split)
+#   train <- x[ind==1,]
+#   test <- x[ind==2,]
+#   return(list(train=train, test=test))
+# }
+# 
+# 
+# # Create random binary data from which to select
+# # observations, `Nsets` is number of 
+# # data sets to create (always keep at 1)
+# # `N` is number of observations, `varnum` is number of variables.
+# # Worth considering increasing `N`. Increasing `varnum` results
+# # in more irrelevant factors later on.
+# varnum <- 18
+# outcome <- "A"
+# base <- as.data.table(bs_dat_create(Nsets = 1, N = 100000, varnum = varnum)[[1]])
+# 
+# 
+# # Set variable names if more vars than length(LETTERS)
+# if (length(LETTERS) < varnum){
+#   names(base) <- c(LETTERS, sapply(1:(varnum-length(LETTERS)), 
+#                                    \(x) paste0("U", x)))
+# }
+# 
+# # Have a look, if you want
+# # head(base)
+# 
+# # Create random binary CNA models for "A",
+# # to be used as targets. 
+# # `relevants`:=number of factors included in the models.
+# relevants <- 15
+# targets <- replicate(40, rasf_hack(LETTERS[1:relevants], 
+#                                     outcome = "A",
+#                                     max.conj = 6,
+#                                     neg.prob = 0.5))
+# 
+# 
+# # Corresponding to each target, create a "clean" data set where 
+# # each observation is some configuration of variable values
+# # that conforms to the target, selected from `base`.
+# # Note that the irrelevant factors
+# # may take any values whatsoever.
+# # The way the targets are created results in ideal data
+# # with extreme outcome prevalence. The function below
+# # corrects for this by oversampling cases with
+# # outcome absent and forcing desired outcome prevalence.
+# alt_SC <- function(mod,data,out,preval){
+#   a <- ct2df(selectCases(mod, data))
+#   n <- nrow(a)
+#   nout <- sum(a[out])
+#   o_prev <- nout / n
+#   if(o_prev > preval){
+#     nn <- preval*n
+#     to_add <- ceiling((nout - nn) / preval)
+#     b <- ct2df(selectCases(mod, data[get(out)==0,]))
+#     neg_outs <- b[sample(1:nrow(b), to_add, replace = TRUE),]
+#     a <- rbind(a, neg_outs)
+#   }
+#   return(a)
+# } 
+# 
+# 
+# #cleandats <- lapply(targets, \(x) ct2df(selectCases(x, base)))
+# 
+# # Noise-free data sets w/ outcome prevalence =<80%
+# cleandats <- lapply(targets, \(x) alt_SC(x, base, outcome,0.8))
+# 
+# # Check that outcome prevalence not zero and discard
+# # data set / target if that is the case
+# check_vars <- function(x){
+#   vars <- unlist(lapply(x, \(y) var(as.numeric(y[,outcome]))))
+#   return(which(vars > 0))
+# }
+# 
+# keep <- check_vars(cleandats)
+# targets <- targets[keep]
+# cleandats <- cleandats[keep]
 
 # Calculate outcome prevalences and discard data sets
 # where outcome prevalence >90%.
@@ -124,18 +124,18 @@ cleandats <- cleandats[keep]
 # from each "clean" data set, create
 # a noisy data set by flipping the outcome value
 # in about 10% of observations
-ndat <- lapply(cleandats, 
-               \(x) flipout(x, outcome = "A", proportion = 0.1))
-
-ndat <- lapply(ndat, \(x){
-  x[[outcome]] <- as.factor(x[[outcome]])
-  return(x)})
-
-# check that outcomes vary, discard if not
-
-keep2 <- check_vars(ndat)
-targets <- targets[keep2]
-ndat <- ndat[keep2]
+# ndat <- lapply(cleandats, 
+#                \(x) flipout(x, outcome = "A", proportion = 0.1))
+# 
+# ndat <- lapply(ndat, \(x){
+#   x[[outcome]] <- as.factor(x[[outcome]])
+#   return(x)})
+# 
+# # check that outcomes vary, discard if not
+# 
+# keep2 <- check_vars(ndat)
+# targets <- targets[keep2]
+# ndat <- ndat[keep2]
 
 # ttsplits <- lapply(ndat, \(x) ttsplit(x, split = c(0.8, 0.2)))
 
@@ -156,6 +156,12 @@ ndat <- ndat[keep2]
 
 # importance(rf)
 # getTree(rf, k=4, labelVar = TRUE)
+datfiles <- list.files(
+  path = "../data",
+  ignore.case = T,
+  full.names = T
+)
+ndat <- lapply(datfiles, \(x) as.data.frame(fread(x)))
 
 treetosuff <- function(model){
   pattern_leq <- "([A-Za-z0-9]*)<0\\.5"
@@ -201,12 +207,13 @@ rf_to_dnf <- function(...){
 
 res <- mclapply(ndat, 
                 \(z) rf_to_dnf(x = z[,-which(names(z)==outcome)],
-                               y = z[,outcome],
-                               maxnodes = 6,
-                               ntree = 180)
+                               y = as.factor(z[,outcome]),
+                               ntree = 140,
+                               maxnodes = 10)
               )
 
-minim <- lapply(res, \(x){
+sapply(res, nchar)
+minim <- mclapply(res, \(x){
   if(nchar(x)>0) rreduce(x) else x
 }) 
   
